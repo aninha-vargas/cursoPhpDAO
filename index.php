39a6a0a018dd56ba8c841167fcd94e1c0527c8a5
Ana Maria Vargas <aninha_vargas@hotmail.com>
2022-01-14 18:15:19 -0300

    listagem php

--- index.php
+++ index.php
@@ -3,15 +3,27 @@
 	require_once("config.php");
 
 	// $sql = new Sql();
-
 	// $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 	// echo json_encode($usuarios);
 
-	$user = new Usuario();
+	
+	// Carrega um usuario
+	// $user = new Usuario();
+	// $user->loadById(3);
+	// echo $user;
 
-	$user->loadById(3);
+	//carrega uma lista de usuarios
+	// $lista = Usuario::getList();
+	// echo json_encode($lista);
 
-	echo $user;
+	// Carrega uma lista de usuarios buscando pelo login
+	// $search = Usuario::search("e");
+	// echo json_encode($search);
+
+	// Carrega um usuario usando o login e a senha
+	$usuario = new Usuario();
+	$usuario->login("teste","@#$");
+	echo $usuario;
+
 
  ?>
