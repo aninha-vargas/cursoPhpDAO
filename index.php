9645cdf28eb4cf41b0f77722c6455fb4df1c4714
Ana Maria Vargas <aninha_vargas@hotmail.com>
2022-01-14 19:19:11 -0300

    insert e update usuarios

--- index.php
+++ index.php
@@ -21,9 +21,26 @@
 	// echo json_encode($search);
 
 	// Carrega um usuario usando o login e a senha
-	$usuario = new Usuario();
-	$usuario->login("teste","@#$");
-	echo $usuario;
+	// $usuario = new Usuario();
+	// $usuario->login("teste","@#$");
+	// echo $usuario;
 
+	// Incluindo usuário no BD usando PROCEDURE - 1ª forma usando - public function insert()
+	// $aluno = new Usuario();
+	// $aluno->setDeslogin("aluno");
+	// $aluno->setDessenha("@lun0");
+	// $aluno->insert();
+	// echo $aluno;
+
+	// Incluindo usuário no BD usando PROCEDURE - 2ª forma usando - public function __construct
+	// $aluno = new Usuario("outroAluno", "202030");
+	// $aluno->insert();
+	// echo $aluno;
+
+	// Alterando usuario no BD
+	$usuario = new Usuario();
+	$usuario->loadById(3);
+	$usuario->update("professor", "a456");
+	echo $usuario;
 
  ?>
